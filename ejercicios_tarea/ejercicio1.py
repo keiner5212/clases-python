9662e5cee4497c0cda0cf01e22f5e8f64ce5345b
Keiner Alvarado <kjgameplay0000@gmail.com>
2023-04-12 22:08:08 -0500

    tarea1

---
+++ ejercicios_tarea/ejercicio1.py
@@ -0,0 +1,12 @@
+# diseña un programa en el que tengas que hacer un menu de 
+# operaciones manejando listas, tienes que hacer el menu con ciclo 
+# for y tambien con while, osea 2 versiones, y el programa 
+# debe permitir hacer distintas operaciones entre los numeros en una lista
+# por ejemplo, teniendo la lista de abajo debes preguntar por las 
+# posiciones que quieres operar
+
+lista=[2,41,4,52,67,47,28,12]
+
+# digamos que quieres operar los 2 primeros numeros, entonces como usuario, 
+# debes decir que los numeros que quieres operar son el de la posicion 0 y el de la 1
+# entonces debes decirle que elija la operacion y mostrar el resultado
