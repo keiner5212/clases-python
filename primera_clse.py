bed85e50cec6fc012cb30d6e1b7984dcff669976
Keiner Alvarado <kjgameplay0000@gmail.com>
2023-04-12 19:02:12 -0500

    first version

---
+++ primera_clse.py
@@ -0,0 +1,21 @@
+#numericos
+entero=5
+decimal=5.65
+
+#cadenas de caracteres
+cadena="hola"
+cadena2='hola2' #indiferente
+
+#colecciones de tipo lista
+tuplas=(1,4,6,3,6)
+listas=[1,4,6,3,6]
+
+#colecciones de tipo objeto o diccionario
+persona={
+    "n":"keiner",
+    "xd e":"alvarado",
+    "a":5
+    }
+
+#booleana
+true=False
