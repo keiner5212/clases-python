eb20ad501e4385f15cc2dd30e6ec436862ebc5ed
Keiner Alvarado <kjgameplay0000@gmail.com>
2023-04-12 19:36:17 -0500

    bucles

---
+++ bucleFor.py
@@ -0,0 +1,5 @@
+
+valor=int(input("Ingresa un numero para mostrar su tabla de multiplicar: "))
+
+for i in range(0, 11):
+    print(valor,"X",i,"=",valor*i)
---
+++ bucleWhile.py
@@ -0,0 +1,4 @@
+
+x=1
+while x%2!=0:
+    x=int(input("ingresa un numero: "))
